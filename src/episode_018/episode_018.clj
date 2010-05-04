64a8fb2632a2d88391f1b4c8fd2b4c11de8b7e0e
Sean <devlinsf@devlinsolutions.com>
2010-05-04 07:47:56 -0400

    Added episode 18

--- src/episode_018/episode_018.clj
+++ src/episode_018/episode_018.clj
@@ -1,6 +1,5 @@
 (ns episode-018
   (:use clojure.test
-	clojure.test.junit
 	clojure.template
 	))
 
@@ -16,21 +15,12 @@ n times."
   ([n expr]
      `(map (fn[~'x] (time* ~expr )) (range 0 ~n))))
 
-(defmacro defspec
-  [name bindings & body]
-  `(def ~name (quote [~bindings ~@body])))
-
-(defmacro enforce [spec & impl]
-  (let [test-name (symbol (apply str (name spec) "-" (map name impl)))]
-    `(let ~(vec (interleave (first (eval spec)) impl))
-       (deftest ~test-name
-	 ~@(rest (eval spec))))))
-
 (def test-vec [1 2 3 4 5])
 
-(defspec rotate-spec
-    [rotate-imp]
-  (are [n] (= (count (rotate-imp n [])) '())
+(declare rotate-imp)
+
+(deftest rotate-spec
+  (are [n] (= (rotate-imp n []) '())
        -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6)
   (are [n] (= (count (rotate-imp n test-vec)) (count test-vec))
        -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6)
@@ -79,19 +69,6 @@ n times."
       (let [[front back] (split-at (mod n (count coll)) coll)] 
 	(concat back front))))
 
-(do-template
- [imp]
- (enforce rotate-spec imp)
- rotate-original
- rotate-lazy-cat
- rotate-concat
- rotate-destruct
- rotate-concat-with-empty
- rotate-destruct-with-empty)
-
-
-(try
- (clojure.core/let [values__6401__auto__ (clojure.core/list 1 1) result__6402__auto__ (clojure.core/apply = values__6401__auto__)]
-		   (if result__6402__auto__
-		     (clojure.test/report {:actual (clojure.core/cons = values__6401__auto__), :type :pass, :message nil, :expected (quote (= 1 1))})
-		     (clojure.test/report {:actual (clojure.core/list (quote not) (clojure.core/cons (quote =) values__6401__auto__)), :type :fail, :message nil, :expected (quote (= 1 1))})) result__6402__auto__) (catch java.lang.Throwable t__6427__auto__ (clojure.test/report {:actual t__6427__auto__, :type :error, :message nil, :expected (quote (= 1 1))})))
+(time* 100
+       (dotimes [x 1000]
+	 (rotate-imp 2 test-vec)))
